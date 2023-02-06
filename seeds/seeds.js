62babfbdd1a78a2101e6c5ed3f2da87e80feda4e
Sarah <sarah.k.hopper@gmail.com>
2023-02-06 00:44:30 -0800

    tweak code in seeds, attempt testing locally

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -7,18 +7,19 @@ const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
+    console.log('\n----- DATABASE SYNCED -----\n');
+
   await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+    console.log('\n----- CATEGORIES SEEDED -----\n');
 
   await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+    console.log('\n----- PRODUCTS SEEDED -----\n');
 
   await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+    console.log('\n----- TAGS SEEDED -----\n');
 
   await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+    console.log('\n----- PRODUCT TAGS SEEDED -----\n');
 
   process.exit(0);
 };
